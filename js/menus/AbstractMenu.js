0a3a61a7656b3dd69f44579d30c810e109b3d7e6
Juliette Sabatier <juliette.sabatier07@gmail.com>
2022-04-28 09:34:21 +0200

    refactor

--- js/menus/AbstractMenu.js
+++ js/menus/AbstractMenu.js
@@ -16,3 +16,72 @@ export default class AbstractMenu extends BABYLON.Scene{
 
     }
 }
+
+
+/*
+    // DropDown to keep if needed
+    
+        levelGui["levelOption"] =  new BABYLON.GUI.Container();
+        levelGui["levelOption"].width = "180px";
+        levelGui["levelOption"].top = "20px";
+        levelGui["levelOption"].left = "600px";
+        levelGui["levelOption"].isHitTestVisible = false;
+
+        // Primary button
+        levelGui["levelOptionButton"] = BABYLON.GUI.Button.CreateSimpleButton(null, "Options");
+        levelGui["levelOptionButton"].height = "40px";
+        levelGui["levelOptionButton"].background = "white";
+        levelGui["levelOptionButton"].color = "black";
+        levelGui["levelOptionButton"].verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_TOP;
+
+        // Options panel
+        levelGui["levelOptionStack"] = new BABYLON.GUI.StackPanel();
+        levelGui["levelOptionStack"].verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_TOP;
+        levelGui["levelOptionStack"].top = "40px";
+        levelGui["levelOptionStack"].isVisible = false;
+        levelGui["levelOptionStack"].isVertical = true;
+
+        // Restart
+        levelGui["restartButton"] = BABYLON.GUI.Button.CreateSimpleButton("restart", "Restart");
+        levelGui["restartButton"].height = "40px";
+        levelGui["restartButton"].paddingTop = "-1px";
+        levelGui["restartButton"].background = "white";
+        levelGui["restartButton"].color = "black";
+        levelGui["levelOptionStack"].addControl(levelGui["restartButton"]);
+
+        // Restart
+        levelGui["save"] = BABYLON.GUI.Button.CreateSimpleButton("Save", "Save");
+        levelGui["save"].height = "40px";
+        levelGui["save"].paddingTop = "-1px";
+        levelGui["save"].background = "white";
+        levelGui["save"].color = "black";
+        levelGui["levelOptionStack"].addControl(levelGui["save"]);
+
+        // Restart
+        levelGui["quit"] = BABYLON.GUI.Button.CreateSimpleButton("quit", "Quit");
+        levelGui["quit"].height = "40px";
+        levelGui["quit"].paddingTop = "-1px";
+        levelGui["quit"].background = "white";
+        levelGui["quit"].color = "black";
+        levelGui["levelOptionStack"].addControl(levelGui["quit"]);
+
+        // Hide and seek stack panel
+        levelGui["levelOptionButton"].onPointerUpObservable.add(function() {
+            levelGui["levelOptionStack"].isVisible = !levelGui["levelOptionStack"].isVisible;
+        });
+
+
+        // add controls
+        levelGui["levelOption"].addControl(levelGui["levelOptionButton"]);
+        levelGui["levelOption"].addControl(levelGui["levelOptionStack"]);
+        levelGui["gui"].addControl(levelGui["levelOption"]);
+
+
+        return levelGui;
+
+    }
+
+    // dropdown find on playground
+    // https://www.babylonjs-playground.com/#H10NI4#3
+
+ */
