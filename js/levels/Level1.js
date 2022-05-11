ec18a885ff337f593e28b2540cb9fcc05d7078fc
Ninjabsent <raphael.pietrzak@etu.univ-cotedazur.fr>
2022-05-11 20:41:31 +0200

    - Added spawn points created in Blender - Reworked the creation of the different spheres - Removed useless files

--- js/levels/Level1.js
+++ js/levels/Level1.js
@@ -19,21 +19,5 @@ export default class Level1 extends AbstractLevel{
         //let ground = this.createGround();
         this.createLights();
         this.buildWalls(1);
-
-        // Sphere 1
-        this.createSphere("player1", 0, 5, 0, 0, new BABYLON.Color3(0, 0, 0)); // rouge
-        // Sphere 2
-        this.createSphere("player2", 1, 5, 245, 245, new BABYLON.Color3(0, 1, 0));  // vert
-        // Sphere 3
-        this.createSphere("player3", 2, 5, 245, -245, new BABYLON.Color3(0, 0, 1)); // bleu
-        // Sphere 4
-        this.createSphere("player4", 3, 5, -245, 245, new BABYLON.Color3(1, 0, 1)); // violet
-        // Sphere 5
-        this.createSphere("player5", 5, 5, -245, -245, new BABYLON.Color3(0, 1, 1)); // cyan
-
-
-        this.activeCamera = this.cameras[0];
-
-
     }
 }
--- js/levels/Level2.js
+++ js/levels/Level2.js
@@ -22,11 +22,6 @@ export default class Level2 extends AbstractLevel{
         this.currentPlayer = 0;
         this.endPosition = new BABYLON.Vector3(0, 15, 250);
 
-        this.createSphere("player6", 0, 5, 0, 0, new BABYLON.Color3(1, 0, 0)); // rouge
-        // Sphere 2
-        this.createSphere("player7", 1, 5, 0, 20, new BABYLON.Color3(0, 1, 0));  // vert
-
-
         this.activeCamera = this.cameras[0];
 
 
