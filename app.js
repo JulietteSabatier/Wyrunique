f0860d4d8ab2e8ca63483addb956f33622f1787d
Yann-Braultr <brlt.yann@gmail.com>
2022-05-14 15:20:44 +0200

    try fixing

--- app.js
+++ app.js
@@ -7,9 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname+'/public/index.html'));
-});
+
 
 app.listen(PORT, () => {
     console.log('App up at port ${PORT}');
