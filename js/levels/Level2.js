5e2e2926d70994e3dd934cd763b252481e995bff
Ninjabsent <raphael.pietrzak@etu.univ-cotedazur.fr>
2022-05-16 18:01:11 +0200

    - Fix texture door level 2

--- js/levels/Level2.js
+++ js/levels/Level2.js
@@ -32,28 +32,7 @@ export default class Level2 extends AbstractLevel{
         let door = this.getMeshByName("Porte1");
         let doorMaterial= new BABYLON.StandardMaterial("doorMaterial", this);
 
-        doorMaterial.diffuseTexture = new BABYLON.Texture("images/doorTexture.jpg", this)
-        doorMaterial.diffuseColor =  new BABYLON.Color3(1,0.5,0);
-        door.material = doorMaterial;
-
-        door.physicsImpostor = new BABYLON.PhysicsImpostor(door,
-            BABYLON.PhysicsImpostor.BoxImpostor, {
-                ignoreParent: true
-            }, this);
-
-        let posButton1 = this.getMeshByName("Button1").position;
-        let button1 = this.createButtonMesh(posButton1, "button1");
-        let posButton2 = this.getMeshByName("Button2").position;
-        let button2 = this.createButtonMesh(posButton2, "button2")
-
-        this.doors[0] = new Door(this, door,[button1,button2]);
-    }
-
-    setButtonAndDoor() {
-        let door = this.getMeshByName("Porte1");
-        let doorMaterial= new BABYLON.StandardMaterial("doorMaterial", this);
-
-        doorMaterial.diffuseTexture = new BABYLON.Texture("images/doorTexture.jpg", this)
+        doorMaterial.diffuseTexture = new BABYLON.Texture("images/Common/doorTexture.jpg", this)
         doorMaterial.diffuseColor =  new BABYLON.Color3(1,0.5,0);
         door.material = doorMaterial;
 
